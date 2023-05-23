8726ece661eb9f4ecdee85de8a1291a7603a95d8
Marcin Regulski <mregulski@outlook.com>
2023-05-23 22:48:01 +0100

    Added width check functions, started iterating through options; added cheatsheet.txt for development

--- simple_menu.sh
+++ simple_menu.sh
@@ -1,23 +1,49 @@
 #!/bin/bash
 
+# Quick notes:
+# - check tput for inputing colours; example = lightblue=$(tput setaf 123)
+# - optimise generateText option for shorter execution (PRIORITY)
+
 # Configuration
 symbol="*"
 padding_symbol=" "
+fg_colour=97
+bg_colour=40
+#bgc="\e${bg_colour}m"
+#fgc="\e${fg_colour}m"
+endc="\e[0m"
 padding_size=4
 line_size=70
+longest_text=0
 
 # checkWidth( options(array) )
-#function checkWidth()
+function checkWidth()
+{
+    local size=0
+    local options=("$@")
+    ## get length of $options array
+    local len=${#options[@]}
+    for (( i=0; i<$len; i++ )); do
+    {
+        local item=${options[i]}
+        ## compare current value to last
+        if [[ ${#item} -gt size ]]; then size=${#item} ; fi
+    }
+    done
+    echo $size 
+}
 
 
 function header()
 {
     generateBorder
-    generateText "This is a test" "header"
+    generateText  
+    generateText "header" " Chyba dzialo "
+    generateText
     generateBorder
 }
 
-# generatePadding(amount)
+# generatePadding( amount )
 function generatePadding()
 {
     local string=""
@@ -33,24 +59,59 @@ function generateBorder()
     for (( i=0; i < line_size; i++ )); do
       string+="$symbol";
     done
-    echo "$string"
+    echo -e "$string"
 }
 
-# generateText (text, type)
+# generateText ( text, type )
 function generateText()
 {
-    local border=1
-    local characters=$line_size
-    local padding=""
-    if [ -n $2 ] && [ $2 = "header" ];
-    then
-            padding_size=$(($line_size / 2 - ${#1} / 2))
-            padding=$(generatePadding $padding_size) 
-            echo "$symbol$padding$1$padding$symbol"
-    fi  
+    local border_width=1
+    local padding_size=0
+    local padding_remaining=0
+    local characters=$(( $line_size - $border_width*2))
+    local padLeft=""
+    local padRight=""
+    local border=$symbol
+    case $1 in
+        "header")
+            padding_size=$(( ($characters - ${#2} ) / 2 ))
+            padding_remaining=$(( $characters - $padding_size - ${#2}))
+            padLeft=$(generatePadding $padding_size)
+            padRight=$(generatePadding $padding_remaining)
+            echo "${gbc}$border$padLeft$2$padRight$border"
+            ;;
+        "option")
+            ;;
+        "settings")
+            ;;
+
+        *)
+            echo "${gbc}$border$(generatePadding $characters)$border"
+            ;;
+    esac
 }
 
-#function generateLine()
+# generateOptions( options(array) )
+function generateOptions()
+{
+    local size=0
+    local options=("$@")
+    local index=1
+    for item in $options 
+    do
+        echo "$index.$item"
+        (($index++))
+    done
+}
 
-clear
-header
+# generateMenu( title, options(array), type )
+function generateMenu()
+{
+    clear
+    header
+    options=("one" "two" "three" "four" "heehehehehe" "one")
+    longest_text=$(checkWidth "${options[@]}")
+}
+
+# Testing ground!
+generateMenu
