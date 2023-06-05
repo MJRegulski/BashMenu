5af34022a44604b8753b6106f810daba2b7bb447
Marcin Regulski <mregulski@outlook.com>
2023-06-06 00:37:21 +0100

    First steps towards XML file reading; parsing values into menu generator.

--- simple_menu.sh
+++ simple_menu.sh
@@ -4,81 +4,78 @@
 # - check tput for inputing colours; example = lightblue=$(tput setaf 123)
 # - optimise generateText option for shorter execution (PRIORITY)
 
+# include xml file script
+source readxml.sh
+
 # Configuration
 symbol="*"
 padding_symbol=" "
-fg_colour=97
-bg_colour=40
-#bgc="\e${bg_colour}m"
-#fgc="\e${fg_colour}m"
-endc="\e[0m"
 padding_size=4
 line_size=70
 longest_text=0
+border_width=1
+border=""
+empty_line=""
 
 # user variables
 declare -a options
 declare title
 
 # checkWidth( options(array) )
-function checkWidth()
-{
+function checkWidth() {
     local size=0
-    local options=("$@")
     ## get length of $options array
     local len=${#options[@]}
-    for (( i=0; i<$len; i++ )); do
-    {
+    for (( i=0; i<$len; i++ )); do 
         local item=${options[i]}
         ## compare current value to last
         if [[ ${#item} -gt size ]]; then size=${#item} ; fi
-    }
     done
-    echo $size 
+    echo $size
 }
 
 # header ( title )
-function header()
-{
-    generateBorder
-    generateText  
+function header() {
+    ## generate known lines
+    border=$(generateBorder)
+    empty_line=$(generateText)  
+    ## echo header
+    echo "$border"
+    echo "$empty_size"
     generateText "header" "$title"
-    generateText
-    generateBorder
+    echo "$empty_size"
+    echo "$border"
 }
 
 # body ( options(array) )
-function body()
-{
-    generateText
+function body() {
+    echo "$empty_line"
     generateOptions
-    generateText
-    generateBorder
+    echo "$empty_line"
+    echo "$border"
 }
 
 # generatePadding( amount )
-function generatePadding()
-{
+function generatePadding() {
     local string=""
     for (( i=0; i < $1; i++ )); do
         string+="$padding_symbol";
     done
+    ## echo the result
     echo "$string"
 }
 
-function generateBorder()
-{
+function generateBorder() {
     local string=""
     for (( i=0; i < line_size; i++ )); do
       string+="$symbol";
     done
+    ## echo the result
     echo -e "$string"
 }
 
 # generateText ( type, text )
-function generateText()
-{
-    local border_width=1
+function generateText() {
     local padding_remaining=0
     local characters=$(( $line_size - $border_width*2))
     local padding=$padding_size
@@ -87,8 +84,7 @@ function generateText()
     local border=$symbol
     if [[ $1 = "header" ]]; then
         padding=$(( ($characters - ${#2} ) / 2 ))
-    fi
-    # used for empty space - echo "$border$(generatePadding $characters)$border"
+    fi 
     ## set up remaining padding
     padding_remaining=$(( $characters - $padding - ${#2}))
     padLeft=$(generatePadding $padding)
@@ -98,31 +94,23 @@ function generateText()
 }
 
 # generateOptions( options(array) )
-function generateOptions()
-{
-    local size=0
+function generateOptions() {
     local index=1
-    for item in "${options[@]}" 
-    do
+    for item in "${options[@]}"; do
         generateText "options" "$index. $item"
         ((index++))
     done
 }
 
 # generateMenu( title, options(array) )
-function generateMenu()
-{
+function generateMenu() {
     ## save user variables
-    options=("${@:2}")
-    title=$1
+    #options=("${@:2}")
+    #title=$1
+    get_menu xml/docker.xml
+    title="${menu[title]}"
     clear
     header
-    body 
-    echo 
+    body  
     #longest_text=$(checkWidth "${options[@]}")
 }
-
-# Testing ground!
-opt=("Siemens" "Allen Bradley" "ABB" "Omron" "Delta" "Honeywell" "GeFanuc" "Schneider Electric" "WAGO")
-plc="Top PLC brands"
-generateMenu "${plc}" "${opt[@]}"
